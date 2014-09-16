88c6b93084a7cce35fffe5cac0af7e122cf4fbef
Brian Birtles <birtles@gmail.com>
2014-09-16 10:07:39 +0900

    Preserve commit user

--- build/travis.sh
+++ build/travis.sh
@@ -11,13 +11,8 @@ grunt build || error_exit "Error building spec"
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   grunt publish || error_exit "Error publishing spec"
 
-  echo "$TRAVIS_COMMIT_RANGE"
-  echo "$TRAVIS_REPO_SLUG"
-  echo "$COMMIT_USER"
-  echo "$COMMIT_EMAIL"
+  git config --global user.name "$COMMIT_USER (via Travis CI)"
+  git config --global user.email "$COMMIT_EMAIL"
 
-  git config --global user.name "webanimbot"
-  git config --global user.email "webanimbot@gmail.com"
-
-  grunt upload --gh-pages-message "$TRAVIS_COMMIT_MSG" || "Error uploading to gh-pages branch"
+  grunt upload --gh-pages-message '$TRAVIS_COMMIT_MSG' || "Error uploading to gh-pages branch"
 fi
