da19a784cebf996b7d2c113aa4dbf899c790c638
Brian Birtles <birtles@gmail.com>
2014-09-16 09:23:04 +0900

    Add git authentication

--- build/travis.sh
+++ build/travis.sh
@@ -14,5 +14,17 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; th
   git config --global user.name "webanimbot"
   git config --global user.email "webanimbot@gmail.com"
 
+  # Git authentication
+  #
+  # The following is based on code from:
+  #
+  #   https://gist.github.com/douglasduteil/5525750
+  #   http://blog.yasuoza.com/2014/01/13/octopress-plus-github-pages-plus-travis/
+  if [ -z "$id_rsa_{1..24}" ]; then echo 'No $id_rsa_{1..24} found !' ; exit 1; fi
+  echo -n $id_rsa_{1..24} >> ~/.ssh/travis_rsa_64
+  base64 --decode --ignore-garbage ~/.ssh/travis_rsa_64 > ~/.ssh/id_rsa
+  chmod 600 ~/.ssh/id_rsa
+  echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+
   grunt upload --gh-pages-message "$TRAVIS_COMMIT_MSG" || "Error uploading to gh-pages branch"
 fi
